1f77e79a8236c53b41e2d5da761b2fdd2c9eb302
Your Name <you@example.com>
2022-06-08 04:41:00 -0700

    day9 completed

--- sampleproject/lib/pages/Home_page.dart
+++ sampleproject/lib/pages/Home_page.dart
@@ -1,21 +1,43 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:sampleproject/models/catalogapp.dart';
 import 'package:sampleproject/widget/drawer.dart';
 
 import '../widget/item_widget.dart';
 
 // ignore: camel_case_types
-class homePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "codekajura";
-
+class homePage extends StatefulWidget {
   const homePage({Key? key}) : super(key: key);
 
   @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  final int days = 30;
+
+  final String name = "codekajura";
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadData();
+  }
+
+  loadData() async {
+    var CatalogJson = await rootBundle.loadString("assets/files/Catalog.json");
+    var decodedData = jsonDecode(CatalogJson);
+    var productsdata = decodedData["products"];
+    CatalogModel.items = List.from(productsdata).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummylist = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
@@ -23,10 +45,10 @@ class homePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-          itemCount: dummylist.length,
+          itemCount: CatalogModel.items.length,
           itemBuilder: (context, index) {
             return ItemWidget(
-              item: dummylist[index],
+              item: CatalogModel.items[index],
             );
           },
         ),
