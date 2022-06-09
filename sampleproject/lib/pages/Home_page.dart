c4c74380bf4b3e826d8ded5ab4966a03dedc4a92
Your Name <you@example.com>
2022-06-09 04:35:46 -0700

    day10 competed

--- sampleproject/lib/pages/Home_page.dart
+++ sampleproject/lib/pages/Home_page.dart
@@ -29,10 +29,13 @@ class _homePageState extends State<homePage> {
   }
 
   loadData() async {
+    await Future.delayed(Duration(seconds: 2));
     var CatalogJson = await rootBundle.loadString("assets/files/Catalog.json");
     var decodedData = jsonDecode(CatalogJson);
     var productsdata = decodedData["products"];
-    CatalogModel.items = List.from(productsdata).map<Item>((item) => Item.fromMap(item)).toList();
+    CatalogModel.items = List.from(productsdata)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
     setState(() {});
   }
 
@@ -44,14 +47,41 @@ class _homePageState extends State<homePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: CatalogModel.items[index],
-            );
+        child: (CatalogModel.items!= null && CatalogModel.items.isNotEmpty)? 
+        GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16
+            ),
+          itemBuilder: (context,index){
+            final Item=CatalogModel.items[index];
+            return Card
+            (
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              child: GridTile(
+                header: Container(
+                  child: Text(Item.name,style: TextStyle(color: Colors.white),),
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                   color: Colors.deepPurple
+                  ),
+                ),
+                child: Image.network(Item.image),
+                footer: Container(
+                  child: Text(Item.price.toString(),style: TextStyle(color: Colors.white),),
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                   color: Colors.black
+                  ),
+                ),
+                ),
+                );
           },
-        ),
+          itemCount: CatalogModel.items.length,)
+        :Center(child: CircularProgressIndicator(),
+      ),
       ),
       drawer: mydrawer(),
     );
